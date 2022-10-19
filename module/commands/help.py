20b1e214be67f1f3d335d7a378894b1d8a166595
InfraBlue <91614116+InfraBlue0@users.noreply.github.com>
2022-10-19 14:20:11 +0200

    feat: added the /help command to the bot

--- module/commands/help.py
+++ module/commands/help.py
@@ -4,4 +4,8 @@ from telegram import Update
 from telegram.ext import CallbackContext
 
 def help_cmd(update: Update, context: CallbackContext) -> None:
-  pass
+  context.bot.sendMessage(chat_id=update.message.chat_id, text=
+    """Telegram ERSU Bot
+    🍽 /menu Fornisce il menù per il prossimo pasto Mensa
+    📚 /ufficioersu Fornisce informazioni sugli uffici ERSU Catania
+    📬 /report Fornisce la possibilità di poter inviare una segnalazione ai Rappresentanti ERSU, riguardante qualsiasi disservizio, informazione, dubbi e domande""")
