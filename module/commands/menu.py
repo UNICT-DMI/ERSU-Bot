264228e1c3b66a7de10005d8d13477eb6db4a343
InfraBlue <91614116+InfraBlue0@users.noreply.github.com>
2022-11-15 12:43:36 +0100

    fix: fixed an import error

--- module/commands/menu.py
+++ module/commands/menu.py
@@ -2,8 +2,6 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
-from module.data.menu_updater import update_menu
-
 def menu(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("") #it will send the current menu based on time, WIP
     return
