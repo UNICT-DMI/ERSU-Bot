83475a98d383f1b503967f2171e17c2b12a7cd18
InfraBlue <91614116+InfraBlue0@users.noreply.github.com>
2022-11-16 12:20:49 +0100

    fix: pylint errors

--- module/commands/menu_settings.py
+++ module/commands/menu_settings.py
@@ -4,9 +4,7 @@ from telegram.ext import CallbackContext
 from module.data.user_settings import UserSettings
 from module.data.menu_settings_buttons import generate_keyboard
 
-def menu_settings(update: Update, context: CallbackContext) -> None:
-    # pylint: disable=unused-argument
-    context = context
+def menu_settings(update: Update, _: CallbackContext) -> None:
     menu_set = UserSettings()
     menu_set.insert_user(update.message.chat_id)
     settings = menu_set.get_user_settings(update.message.chat_id)
--- module/commands/report.py
+++ module/commands/report.py
@@ -1,3 +1,4 @@
+# pylint: skip-file
 """/report command"""
 
 from telegram import Update
@@ -21,17 +22,17 @@ def report(update: Update, context: CallbackContext) -> None:
         if context.args:
             message = "⚠ Report ⚠\n"\
                         f"Username: @{chat_user.username}\n"
-#
-#            if chat_user.first_name is not None:
-#                message += f"Nome/Name: {chat_user.first_name}\n"
-#            if chat_user.last_name is not None:
-#                message += f"Cognome/Surname: {chat_user.last_name}\n"
-#
-#            message += f"Segnalazione/Content: {' '.join(context.args)}\n"
-#
-#            context.bot.sendMessage(chat_id=config_map['representatives_group'], text=message)
-#            context.bot.sendMessage(chat_id=chat_id,
-#                                    text=get_locale(locale, TEXT_IDS.REPORT_RESPONSE_TEXT_ID).replace(PLACE_HOLDER, message))
-#        else:
-#            context.bot.sendMessage(chat_id=chat_id,
-#                                    text=get_locale(locale, TEXT_IDS.REPORT_WARNING_TEXT_ID))
+
+            if chat_user.first_name is not None:
+                message += f"Nome/Name: {chat_user.first_name}\n"
+            if chat_user.last_name is not None:
+                message += f"Cognome/Surname: {chat_user.last_name}\n"
+
+            message += f"Segnalazione/Content: {' '.join(context.args)}\n"
+
+            context.bot.sendMessage(chat_id=config_map['representatives_group'], text=message)
+            context.bot.sendMessage(chat_id=chat_id,
+                                    text=get_locale(locale, TEXT_IDS.REPORT_RESPONSE_TEXT_ID).replace(PLACE_HOLDER, message))
+        else:
+            context.bot.sendMessage(chat_id=chat_id,
+                                    text=get_locale(locale, TEXT_IDS.REPORT_WARNING_TEXT_ID))
