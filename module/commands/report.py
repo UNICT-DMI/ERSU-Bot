138c02b059e0cfcaa98ee69663507eb34e389647
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-11-17 22:38:29 +0100

    refactor: minor import and formatter changes

--- module/commands/report.py
+++ module/commands/report.py
@@ -1,8 +1,8 @@
 """/report command"""
 from telegram import Update
 from telegram.ext import CallbackContext
-
 from module.shared import config_map
+
 
 def report(update: Update, context: CallbackContext) -> None:
     """Called by the /report command.
@@ -18,21 +18,22 @@ def report(update: Update, context: CallbackContext) -> None:
 
     if not chat_user.username:
         context.bot.sendMessage(chat_id=chat_id, text="Errore! Imposta un username!")
+        return
+
+    if context.args:
+        message = "⚠ Report ⚠\n" f"id: {chat_id}\nUsername: @{chat_user.username}\n"
+
+        if chat_user.first_name is not None:
+            message += f"Nome: {chat_user.first_name}\n"
+        if chat_user.last_name is not None:
+            message += f"Cognome: {chat_user.last_name}\n"
+
+        message += f"Segnalazione:\n{user_report_message[8:]}\n"
+
+        context.bot.sendMessage(chat_id=config_map["admin_group"], text=message)
+        context.bot.sendMessage(chat_id=chat_id, text="Segnalazione inviata!")
     else:
-        if context.args:
-            message = "⚠ Report ⚠\n" f"id: {chat_id}\nUsername: @{chat_user.username}\n"
-
-            if chat_user.first_name is not None:
-                message += f"Nome: {chat_user.first_name}\n"
-            if chat_user.last_name is not None:
-                message += f"Cognome: {chat_user.last_name}\n"
-
-            message += f"Segnalazione:\n{user_report_message[8:]}\n"
-
-            context.bot.sendMessage(chat_id=config_map["admin_group"], text=message)
-            context.bot.sendMessage(chat_id=chat_id, text="Segnalazione inviata!")
-        else:
-            context.bot.sendMessage(
-                chat_id=chat_id,
-                text="Errore. Invia un report con\n/report <messaggio>",
-            )
+        context.bot.sendMessage(
+            chat_id=chat_id,
+            text="Errore. Invia un report con\n/report <messaggio>",
+        )
--- module/data/__init__.py
+++ module/data/__init__.py
@@ -1,6 +1,30 @@
 """Data storage and constants"""
 from .user_settings import UserSettings
 from .news import News
+from .constants import (
+    SYMBOLS,
+    EMPTY,
+    USER_EMPTY_SETTINGS,
+    VALID_DAYS,
+    DAYS_TRANSLATION,
+    DB_PATH,
+    CHECK,
+    CONTACT_ERSU,
+    CROSS,
+    DAYS,
+    DAYS_MEAL_REGEX,
+    ERSU_LINK_REGEX,
+    HELP,
+    HELP_CMD_TEXT,
+    MEALS,
+    MENU_MENSA,
+    MENU_SETTINGS,
+    REPORT,
+    START_CMD_TEXT,
+    UFFICIO_ERSU_CMD_TEXT,
+    VALID_MEALS,
+)
+
 
 def setup_db() -> None:
     UserSettings().setup()
