5580297b1e2c7c8591a5ece62dff9cc8bd9ff397
InfraBlue <91614116+InfraBlue0@users.noreply.github.com>
2022-11-17 08:05:45 +0100

    Revert "feat: added support for reporting (/report and /reply)"
    This reverts commit 1665a36e6a63b8e451f3b2ef1251955a590846d5.

--- module/commands/reply.py
+++ module/commands/reply.py
@@ -22,7 +22,8 @@ def reply(update: Update, context: CallbackContext) -> None:
         context.bot.sendMessage(chat_id=chat_id, text="Errore! Solo gli amministratori possono rispondere ai messaggi!")
     else:
         if context.args:
-            context.bot.sendMessage(chat_id=reply_chat_id, text="Messaggio dal rappresentante:\n" + (' '.join(context.args)))
+            context.bot.sendMessage(chat_id=reply_chat_id, text="Messaggio dal rappresentante:")
+            context.bot.sendMessage(chat_id=reply_chat_id, text=(' '.join(context.args)))
         else:
             context.bot.sendMessage(
                 chat_id=chat_id,
