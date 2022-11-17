0c89d76119e4fe1c65aa9ade0d9801091a44830c
Ernesto Casablanca <casablancaernesto@gmail.com>
2022-11-17 22:40:24 +0100

    refactor(db): change the database queries for user_settings

--- module/data/user_settings.py
+++ module/data/user_settings.py
@@ -1,52 +1,44 @@
 """Database class"""
 import sqlite3
-from .constants import DB_PATH
-from .constants import DAYS, MEALS, VALID_DAYS, VALID_MEALS
+from .constants import DB_PATH, DAYS, MEALS, VALID_DAYS, VALID_MEALS, USER_EMPTY_SETTINGS
+
 
 class UserSettings:
     """
-        A class to easy access the database and storing all user preferences
+    A class to easy access the database and storing all user preferences
     """
-    def row_factory(self, _ , row) -> list:
+
+    def row_factory(self, _, row) -> list:
         return row if len(row) != 1 else row[0]
 
     def setup(self) -> None:
         with sqlite3.connect(DB_PATH) as con:
             cur = con.cursor()
-            cur.execute("""CREATE TABLE IF NOT EXISTS user_settings (
+            cur.execute(
+                """CREATE TABLE IF NOT EXISTS user_settings (
                 chat_id INTEGER PRIMARY KEY,
-                monday_lunch INTEGER(1) NOT NULL DEFAULT 1,
-                monday_dinner INTEGER(1) NOT NULL DEFAULT 0,
+                monday_lunch INTEGER(1) NOT NULL DEFAULT 0 CHECK (monday_lunch IN (0, 1)),
+                monday_dinner INTEGER(1) NOT NULL DEFAULT 0 CHECK (monday_dinner IN (0, 1)),
 
-                tuesday_lunch INTEGER(1) NOT NULL DEFAULT 1,
-                tuesday_dinner INTEGER(1) NOT NULL DEFAULT 0,
+                tuesday_lunch INTEGER(1) NOT NULL DEFAULT 0 CHECK (tuesday_lunch IN (0, 1)),
+                tuesday_dinner INTEGER(1) NOT NULL DEFAULT 0 CHECK (tuesday_dinner IN (0, 1)),
 
-                wednesday_lunch INTEGER(1) NOT NULL DEFAULT 1,
-                wednesday_dinner INTEGER(1) NOT NULL DEFAULT 0,
+                wednesday_lunch INTEGER(1) NOT NULL DEFAULT 0 CHECK (wednesday_lunch IN (0, 1)),
+                wednesday_dinner INTEGER(1) NOT NULL DEFAULT 0 CHECK (wednesday_dinner IN (0, 1)),
 
-                thursday_lunch INTEGER(1) NOT NULL DEFAULT 1,
-                thursday_dinner INTEGER(1) NOT NULL DEFAULT 0,
+                thursday_lunch INTEGER(1) NOT NULL DEFAULT 0 CHECK (thursday_lunch IN (0, 1)),
+                thursday_dinner INTEGER(1) NOT NULL DEFAULT 0 CHECK (thursday_dinner IN (0, 1)),
 
-                friday_lunch INTEGER(1) NOT NULL DEFAULT 1,
-                friday_dinner INTEGER(1) NOT NULL DEFAULT 0,
+                friday_lunch INTEGER(1) NOT NULL DEFAULT 0 CHECK (friday_lunch IN (0, 1)),
+                friday_dinner INTEGER(1) NOT NULL DEFAULT 0 CHECK (friday_dinner IN (0, 1)),
 
-                saturday_lunch INTEGER(1) NOT NULL DEFAULT 0,
-                saturday_dinner INTEGER(1) NOT NULL DEFAULT 0,
+                saturday_lunch INTEGER(1) NOT NULL DEFAULT 0 CHECK (saturday_lunch IN (0, 1)),
+                saturday_dinner INTEGER(1) NOT NULL DEFAULT 0 CHECK (saturday_dinner IN (0, 1)),
 
-                sunday_lunch INTEGER(1) NOT NULL DEFAULT 0,
-                sunday_dinner INTEGER(1) NOT NULL DEFAULT 0
-                )""")
-
-    def insert_user(self, chat_id:int) -> None:
-        with sqlite3.connect(DB_PATH) as con:
-            cur = con.cursor()
-            cur.execute("""SELECT chat_id
-            FROM user_settings
-            WHERE chat_id = (?)""", (chat_id, ))
-
-            user_exists = cur.fetchall()
-            if len(user_exists) == 0:
-                cur.execute("INSERT INTO user_settings (chat_id) values (?)", (chat_id, ))
+                sunday_lunch INTEGER(1) NOT NULL DEFAULT 0 CHECK (sunday_lunch IN (0, 1)),
+                sunday_dinner INTEGER(1) NOT NULL DEFAULT 0 CHECK (sunday_dinner IN (0, 1))
+                )"""
+            )
 
     def set_meal(self, chat_id: int, day: DAYS, meal: MEALS) -> None:
         if day not in VALID_DAYS:
@@ -54,47 +46,39 @@ class UserSettings:
         if meal not in VALID_MEALS:
             raise ValueError("Expected meal to be lunch or dinner")
 
-        day_meal = day + '_' + meal
+        day_meal = f"{day}_{meal}"
 
         with sqlite3.connect(DB_PATH) as con:
             cur = con.cursor()
-            cur.execute(f"""SELECT {day_meal}
-            FROM user_settings
-            WHERE chat_id = {chat_id}
-            """)
-            old_meal = cur.fetchall()
-
-            if old_meal == [(0, )]:
-                cur.execute(f"""UPDATE user_settings
-                SET {day_meal} = 1
-                WHERE chat_id = {chat_id}
-                """)
-            if old_meal == [(1, )]:
-                cur.execute(f"""UPDATE user_settings
-                SET {day_meal} = 0
-                WHERE chat_id = {chat_id}
-                """)
+            cur.execute(
+                f"INSERT INTO user_settings (chat_id, {day_meal}) values (?, 1)"
+                f"ON CONFLICT(chat_id) DO UPDATE SET {day_meal} = 1 - user_settings.{day_meal}",
+                (chat_id,),
+            )
 
     def get_users(self) -> list[int]:
         with sqlite3.connect(DB_PATH) as con:
             con.row_factory = self.row_factory
             cur = con.cursor()
-            result = cur.execute("""SELECT chat_id
+            result = cur.execute(
+                """SELECT chat_id
             FROM user_settings
-            """)
+            """
+            )
             return result.fetchall()
 
-    def get_user_settings(self, chat_id: int) -> list:
+    def get_user_settings(self, chat_id: int) -> list[int]:
         with sqlite3.connect(DB_PATH) as con:
             cur = con.cursor()
-            result = cur.execute("""SELECT *
+            result = cur.execute(
+                """SELECT *
             FROM user_settings
             WHERE chat_id = (?)
-            """, (chat_id, )
+            """,
+                (chat_id,),
             )
-            tmp = (result.fetchall())[0]
-            settings = [tmp[var+1] for var in range(14)]
-            return settings
+            settings = result.fetchone()
+            return settings[1:] if settings else USER_EMPTY_SETTINGS
 
     def get_users_to_notify(self, day: DAYS, meal: MEALS) -> list[int]:
         if day not in VALID_DAYS:
@@ -102,26 +86,26 @@ class UserSettings:
         if meal not in VALID_MEALS:
             raise ValueError("Expected meal to be lunch or dinner")
 
-        day_meal = day + '_' + meal
+        day_meal = f"{day}_{meal}"
 
         with sqlite3.connect(DB_PATH) as con:
             con.row_factory = self.row_factory
             cur = con.cursor()
-            result = cur.execute("""SELECT chat_id
+            result = cur.execute(
+                """SELECT chat_id
             FROM user_settings
             WHERE (?) = 1
-            """, (day_meal, ))
+            """,
+                (day_meal,),
+            )
             return result.fetchall()
 
     def delete_user(self, chat_id: int) -> None:
         with sqlite3.connect(DB_PATH) as con:
             cur = con.cursor()
-            cur.execute("""DELETE FROM user_settings
+            cur.execute(
+                """DELETE FROM user_settings
                 WHERE chat_id = (?)
-                """, (chat_id, ))
-
-    def reset_user_settings(self, chat_id: int) -> None:
-        with sqlite3.connect(DB_PATH) as con:
-            cur = con.cursor()
-            cur.execute("""REPLACE INTO user_settings
-            values ((?), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)""", (chat_id, ))
+                """,
+                (chat_id,),
+            )
