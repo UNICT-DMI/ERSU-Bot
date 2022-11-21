c5970889def1750057c6a9039ccb1e06daf5e87b
InfraBlue <91614116+InfraBlue0@users.noreply.github.com>
2022-11-21 14:27:17 +0100

    fix: fixed problem with fast replies

--- module/commands/reply.py
+++ module/commands/reply.py
@@ -4,6 +4,7 @@ from telegram import Update
 from telegram.ext import CallbackContext
 from module.data.constants import CHAT_ID_REGEX
 
+
 def reply(update: Update, context: CallbackContext) -> None:
     """Called by the /reply command.
     Use: to a report message reply -> /reply <word> ...
@@ -12,25 +13,35 @@ def reply(update: Update, context: CallbackContext) -> None:
         update: update event
         context: context passed by the handler
     """
-
     chat_id = update.message.chat_id
     admin_reply_message = update.message.text
 
+    # Reply must be to a bot's message
+    if update.message.reply_to_message.from_user.id != context.bot.id:
+        return
+
+    # Message must contain text
     if update.message.reply_to_message.text is None:
         context.bot.sendMessage(
-                chat_id=chat_id, text="Non puoi rispondere a questo tipo di messaggio!")
+            chat_id=chat_id, text="Non puoi rispondere a questo tipo di messaggio!"
+        )
         return
 
     groups = re.search(CHAT_ID_REGEX, update.message.reply_to_message.text)
+    # Message must contain an id in the message (the second group is the chat_id)
     if groups is None:
         return
 
     reply_chat_id = groups.group(2)
-    if context.args:
-        context.bot.sendMessage(
-            chat_id=reply_chat_id,
-            text=f"Messaggio dal rappresentante:\n{admin_reply_message[7:]}",
-        )
+    if update.message.text.startswith("/reply"):
+        if context.args:
+            context.bot.sendMessage(
+                chat_id=reply_chat_id,
+                text=f"Messaggio dal rappresentante:\n{admin_reply_message[7:]}",
+            )
+        else:
+            context.bot.sendMessage(chat_id=chat_id, text="Per rispondere ad un messaggio utilizza /reply <messaggio>.")
+            return
     else:
         context.bot.sendMessage(
             chat_id=reply_chat_id,
