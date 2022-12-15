2654ed8c0c60affd9a5d446f1545b66f6b24dbf4
DefEnge <59794331+DefEnge@users.noreply.github.com>
2022-12-15 22:21:53 +0100

    refactor code and logic basics

--- module/scraper/scraper.py
+++ module/scraper/scraper.py
@@ -1,4 +1,6 @@
 """"scraper module"""
+from datetime import datetime as time
+import locale
 from bs4 import BeautifulSoup
 import requests
 import yaml
@@ -13,11 +15,11 @@ def get_html(url):
 
 def find_info(articles_list: list, article_number: int):
     #finds all the info of an article
-    last_article = articles_list[article_number]
-    href_article = last_article.find_all('a',href=True, title=True)
+    article = articles_list[article_number]
+    href_article = article.find_all('a',href=True, title=True)
     title_article = href_article[1].get_text()
-    div_article = last_article.find('div', {'class' : 'slide-content'}).find('div',{'class' : 'slide-meta' })
-    link_article = last_article.find('a')
+    div_article = article.find('div',{'class' : 'slide-meta' })
+    link_article = article.find('a')
     link_article = link_article['href']
     if div_article is not None:
         time_article = div_article.find('time').get_text()
@@ -40,18 +42,43 @@ def find_info(articles_list: list, article_number: int):
         content_article = None
 
     return title_article , time_article, link_article, tag_article, content_article
-    #you can use the code below to check if the data scraped are right
-    #print(title_article, "\n", time_article, "\n", link_article, "\n" , tag_article, "\n", content_article, "\n" )
+
+def check_db(title_article: str ,time_article: str):
+    print(title_article, "\n" ,time_article)
+    #if title not present inside table return true
+
+
+def publish_article():
+    print("prova")
+
+def find_tester() -> None:
+    for i in range(15):
+        article_info=find_info(articles, i)
+        print(article_info)
+        print("article: ", i)
+        print("\n")
 
 url_ersu = data['url_ersu']
 url_html = get_html(url_ersu)
+date = time.now()
+locale.setlocale(locale.LC_TIME, "it_IT.UTF-8") #converts the date in to another lang
+date_formatted = date.strftime("%-d %B %Y") #formats date
 
 soup = BeautifulSoup(url_html, 'html.parser')
-articles = soup.find_all('article') #it gives a list of all the article tags
+articles = soup.find_all('article')
+last_articles = find_info(articles,0)
+j=0
 
-for i in range(15):
-    article_info=find_info(articles, i)
-    print(article_info)
-    print("article: ", i)
-    print("\n")
-    
+while True:
+    j = j + 1
+    tms_article = last_articles[1]
+    title = last_articles[0]
+    last_articles = find_info(articles,j)
+
+    if  tms_article != date_formatted:
+        print("esco")
+        j=0
+        break
+
+    if check_db(title,tms_article):
+        publish_article()
