184f0296c94f57157c01330820a5c0809ad15087
DefEnge <59794331+DefEnge@users.noreply.github.com>
2023-03-06 16:22:49 +0100

    initial menu commit

--- module/menuparser/menu.py
+++ module/menuparser/menu.py
@@ -2,8 +2,9 @@ import openpyxl
 from openpyxl import load_workbook
 
 #temporary way to acces the file necessary to extrapoalte the menù
-#MENU_PATH = 'Menù settimanale pranzo.xlsx'
+MENU_PATH = 'Menù settimanale pranzo.xlsx'
 
+KEY_COLUMN = {1 : "B", 2 : "C" , 3 : "D" , 4 :  "E", 5 : "F" , 6 : "G", 7 : "H"}
 
 #pass the menu of choice to open it
 def open_menu(menu_name:str) -> openpyxl.worksheet.worksheet.Worksheet:
@@ -15,13 +16,15 @@ def open_menu(menu_name:str) -> openpyxl.worksheet.worksheet.Worksheet:
 
 #these functions returns the menu from the chosen day
 
-def extrapolate_menu(w_s: openpyxl.worksheet.worksheet.Worksheet, table_colum:  str) -> list:
+def extrapolate_menu(w_s: openpyxl.worksheet.worksheet.Worksheet, table_colum:  int) -> list:
 
     menu_of_the_day = []
+    table_char = KEY_COLUMN[table_colum]
+
 
     for i in range (4, 15):
         i_str = str(i)
-        current_course = w_s[table_colum + i_str].value
+        current_course = w_s[table_char + i_str].value
         menu_of_the_day.append(current_course)
 
 
@@ -29,35 +32,44 @@ def extrapolate_menu(w_s: openpyxl.worksheet.worksheet.Worksheet, table_colum:  
     return menu_of_the_day
 
 def scroll_table(w_s , day:int) -> list:
-
     match day:
 
         case 1:
-            menu_of_the_day = extrapolate_menu(w_s, "B")
+            menu_of_the_day = extrapolate_menu(w_s, day)
 
         case 2:
-            menu_of_the_day = extrapolate_menu(w_s, "C")
+            menu_of_the_day = extrapolate_menu(w_s, day)
 
         case 3:
-            menu_of_the_day = extrapolate_menu(w_s, "D")
+            menu_of_the_day = extrapolate_menu(w_s, day)
 
         case 4:
-            menu_of_the_day = extrapolate_menu(w_s, "E")
+            menu_of_the_day = extrapolate_menu(w_s, day)
 
         case 5:
-            menu_of_the_day = extrapolate_menu(w_s, "F")
+            menu_of_the_day = extrapolate_menu(w_s, day)
 
         case 6:
-            menu_of_the_day = extrapolate_menu(w_s, "G")
+            menu_of_the_day = extrapolate_menu(w_s, day)
 
         case 7:
-            menu_of_the_day = extrapolate_menu(w_s, "H")
+            menu_of_the_day = extrapolate_menu(w_s, day)
 
         case _:
             return "ERROR the day selected doesn't exist"
 
     return menu_of_the_day
 
+
+#fucntion for testing
+
+def print_function(menu_list: list) -> None:
+    for i in range(len(menu_list)):
+        print(menu_list[i])
+
+
 #Calls to the functions for testing
-#table = open_menu(MENU_PATH)
-#menu = scroll_table(table, 1)
+table = open_menu(MENU_PATH)
+menu = scroll_table(table, 1)
+
+print_function(menu)
